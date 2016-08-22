c6f3f9c40d30208095d411ed3ae9e996ccf41e71
Theking530 <amine@suburbandigital.net>
2016-08-22 05:13:01 -0400

    a

--- src/main/java/theking530/staticpower/items/tools/SolderingIron.java
+++ src/main/java/theking530/staticpower/items/tools/SolderingIron.java
@@ -8,26 +8,6 @@ import theking530.staticpower.StaticPower;
 import theking530.staticpower.items.ItemBase;
 import theking530.staticpower.utils.EnumTextFormatting;
 
-<<<<<<< HEAD
-public class SolderingIron extends ItemBase {
-	
-		
-	public SolderingIron(String name, int maxUses) {
-		super(name);
-		setMaxStackSize(1);
-		setMaxDamage(maxUses);
-		setNoRepair();
-	}
-
-	@Override  
-	public void addInformation(ItemStack itemstack, EntityPlayer player, List list, boolean par4) {
-	    	if(showHiddenTooltips()) {
-	    		list.add("Max Uses: " + getMaxDamage());
-	    		list.add("Uses Remaining: " + (getMaxDamage() - getDamage(itemstack)));
-	    	}else{
-	    		list.add(EnumTextFormatting.ITALIC + "Hold Shift");
-	    }
-=======
 public class SolderingIron extends ItemBase implements ISolderingIron{
 	
 	public SolderingIron(String name, int maxUses) {
@@ -57,6 +37,5 @@ public class SolderingIron extends ItemBase implements ISolderingIron{
 	@Override
 	public boolean canSolder(ItemStack stack) {
 		return true;
->>>>>>> branch '1.10.2' of https://github.com/Theking5301/StaticPower.git
 	}
 }
--- src/main/java/theking530/staticpower/machines/solderingtable/TileEntitySolderingTable.java
+++ src/main/java/theking530/staticpower/machines/solderingtable/TileEntitySolderingTable.java
@@ -1,32 +1,6 @@
 package theking530.staticpower.machines.solderingtable;
 
 import net.minecraft.entity.player.EntityPlayer;
-<<<<<<< HEAD
-import net.minecraft.inventory.InventoryCrafting;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.EnumHand;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.player.PlayerDestroyItemEvent;
-import theking530.staticpower.machines.BaseTileEntity;
-import theking530.staticpower.utils.StaticMath;
-
-public class TileEntitySolderingTable extends BaseTileEntity {
-		
-	private int SOLDERING_IRON_DAMAGE = 1;
-	
-	public TileEntitySolderingTable() {
-		initializeBasicTileEntity(45, null, null);
-	}
-	@Override
-	public String getName() {
-		return "SolderingTable";		
-	}
-	public void onCrafted(EntityPlayer player, ItemStack item, int amount) {
-		MinecraftForge.EVENT_BUS.post(new PlayerDestroyItemEvent(player, slots[18], EnumHand.MAIN_HAND));
-		slots[18].setItemDamage(slots[18].getItemDamage() + SOLDERING_IRON_DAMAGE);
-		if(slots[18].getItemDamage() >= slots[18].getMaxDamage()) {
-			slots[18] = null;
-=======
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumHand;
 import net.minecraftforge.common.MinecraftForge;
@@ -52,7 +26,6 @@ public class TileEntitySolderingTable extends BaseTileEntity {
 				ISolderingIron tempIron = (ISolderingIron) slots[18].getItem();
 				tempIron.useSolderingItem(slots[18]);		
 			}
->>>>>>> branch '1.10.2' of https://github.com/Theking5301/StaticPower.git
 		}
     	boolean flag = false;
 		for (int i = 0; i < 9; ++i){
